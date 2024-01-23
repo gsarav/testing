4e03d18f0c573ebc6d7cb15d36f4bbf56d8ae7de
gsarav <107950808+gsarav@users.noreply.github.com>
2024-01-23 12:10:04 -0500

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
     try {
         mongoose.set('strictQuery', false);
-        const conn = await mongoose.connect(process.env.MONGO_URI);
+        const conn = await mongoose.connect('mongodb+srv://gsarav:EntTzGPBM8eADEHx@cluster0.zpvb9fq.mongodb.net/UMDConnect');
         console.log(`Database connected: ${conn.connection.host}`);
     } catch (error){
         console.log(error);
